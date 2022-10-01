94f196b17d9901f88b794eae164aac48db7843f7
Vu Van Dung <me@joulev.dev>
2022-10-02 01:22:09 +0800

    add anchor to component list
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/anime.joulev.dev/src/lib/components/a.svelte
+++ apps/anime.joulev.dev/src/lib/components/a.svelte
@@ -16,22 +16,3 @@
 >
   <slot />
 </a>
-
-<style lang="postcss">
-  .anchor {
-    background-image: linear-gradient(theme(colors.main.300), theme(colors.main.300)),
-      linear-gradient(to right, theme(colors.main.600), theme(colors.main.600));
-    @apply bg-no-repeat transition-all;
-    background-size: 100% 1px, 0 1px;
-    background-position: 100% 100%, 0% 100%;
-  }
-  @media (prefers-color-scheme: dark) {
-    .anchor {
-      background-image: linear-gradient(theme(colors.main.700), theme(colors.main.700)),
-        linear-gradient(to right, theme(colors.main.400), theme(colors.main.400));
-    }
-  }
-  .anchor:hover {
-    background-size: 0 1px, 100% 1px;
-  }
-</style>
--- packages/theme/src/plugin.ts
+++ packages/theme/src/plugin.ts
@@ -1,12 +1,30 @@
 import internal from "tailwindcss/plugin";
 
 export const plugin = internal(function ({ addComponents, theme }) {
+  const main = {
+    300: theme("colors.main.300"),
+    400: theme("colors.main.400"),
+    500: theme("colors.main.500"),
+    600: theme("colors.main.600"),
+    700: theme("colors.main.700"),
+  };
   addComponents({
     ".muted": {
-      color: theme("colors.main.400"),
+      color: main[400],
+      "@media (prefers-color-scheme: dark)": { color: main[500] },
+    },
+    ".anchor": {
+      backgroundImage: `linear-gradient(${main[300]}, ${main[300]}), linear-gradient(to right, ${main[600]}, ${main[600]})`,
+      backgroundSize: "100% 1px, 0 1px",
+      backgroundPosition: "100% 100%, 0% 100%",
+      backgroundRepeat: "no-repeat",
+      transitionProperty: "all",
+      transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+      transitionDuration: "150ms",
       "@media (prefers-color-scheme: dark)": {
-        color: theme("colors.main.500"),
+        backgroundImage: `linear-gradient(${main[700]}, ${main[700]}), linear-gradient(to right, ${main[400]}, ${main[400]})`,
       },
+      "&:hover": { backgroundSize: "0 1px, 100% 1px" },
     },
   });
 });
