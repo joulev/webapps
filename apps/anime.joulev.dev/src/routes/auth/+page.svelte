c53bad17cf92bfe129850ac635f77715d7a72ff6
Vu Van Dung <me@joulev.dev>
2022-10-03 01:51:38 +0800

    extract first line of commit message to send to Vercel
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/anime.joulev.dev/src/routes/auth/+page.svelte
+++ apps/anime.joulev.dev/src/routes/auth/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import { dev } from "$app/environment";
   import { auth } from "$lib/stores/user";
-
   import Button from "$lib/components/button.svelte";
   import Loading from "$lib/components/loading.svelte";
 
