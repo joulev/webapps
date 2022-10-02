cc6a7b8bd37459578da99cf99653c01c4e9639d1
Vu Van Dung <me@joulev.dev>
2022-10-03 02:51:41 +0800

    conditionally install python for apps/website
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -2,6 +2,7 @@ import { motion, useAnimationControls } from "framer-motion";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+
 import "~/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
