7cf33c987ab8f01f127d30ab4bfad00a493376d9
Vu Van Dung <me@joulev.dev>
2022-10-17 02:35:54 +0800

    [apps/link] attempt to use tree-shakable es import for validator
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/link/server/lib/validator.ts
+++ apps/link/server/lib/validator.ts
@@ -1,5 +1,5 @@
-import __isSlug from "validator/lib/isSlug";
-import __isURL from "validator/lib/isURL";
+import __isSlug from "validator/es/lib/isSlug";
+import __isURL from "validator/es/lib/isURL";
 
 export const isSlug = (value: string) => __isSlug(value);
 export const isURL = (value: string) => __isURL(value, { require_protocol: true });
