5c22a6ebde3c711189088e10b92a70f16a1c8d7a
Vu Van Dung <me@joulev.dev>
2022-10-24 23:15:54 +0800

    [apps/link] add some more restrictions to slug matching
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/link/lib/validator.ts
+++ apps/link/lib/validator.ts
@@ -1,5 +1,6 @@
 import __isSlug from "validator/es/lib/isSlug";
 import __isURL from "validator/es/lib/isURL";
 
-export const isSlug = (value: string) => __isSlug(value);
+export const isSlug = (value: string) =>
+  __isSlug(value) && value.length <= 50 && /^[a-zA-Z0-9-]+$/.test(value);
 export const isURL = (value: string) => __isURL(value, { require_protocol: true });
