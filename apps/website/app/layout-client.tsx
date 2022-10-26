a043d9e9307230a3b50c60096828e60c08014c98
Vu Van Dung <me@joulev.dev>
2022-10-27 03:29:10 +0800

    [apps/website] try the new vercel/analytics package
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/website/app/layout-client.tsx
+++ apps/website/app/layout-client.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { motion, useAnimationControls } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function LayoutClient({ children }: React.PropsWithChildren) {
   const controls = useAnimationControls();
@@ -10,13 +11,16 @@ export default function LayoutClient({ children }: React.PropsWithChildren) {
     // controls.start("hidden").then(() => controls.start("visible")); // Probably will need this in the future
   }, [controls]);
   return (
-    <motion.div
-      className="container max-w-screen-md"
-      initial="hidden"
-      animate={controls}
-      variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
-    >
-      {children}
-    </motion.div>
+    <>
+      <motion.div
+        className="container max-w-screen-md"
+        initial="hidden"
+        animate={controls}
+        variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
+      >
+        {children}
+      </motion.div>
+      <Analytics />
+    </>
   );
 }
