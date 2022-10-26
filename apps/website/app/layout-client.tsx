bd796b3dd30a2e08fc29f21f2a7c18ce3fb6ee8e
Vu Van Dung <me@joulev.dev>
2022-10-27 03:18:32 +0800

    [apps/website] fix weird bug of internal server error
    Signed-off-by: Vu Van Dung <me@joulev.dev>

---
+++ apps/website/app/layout-client.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+import { motion, useAnimationControls } from "framer-motion";
+
+export default function LayoutClient({ children }: React.PropsWithChildren) {
+  const controls = useAnimationControls();
+  useEffect(() => {
+    controls.start("visible");
+    // controls.start("hidden").then(() => controls.start("visible")); // Probably will need this in the future
+  }, [controls]);
+  return (
+    <motion.div
+      className="container max-w-screen-md"
+      initial="hidden"
+      animate={controls}
+      variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,34 +1,16 @@
-"use client";
-
-import { FC, PropsWithChildren, useEffect } from "react";
-import { motion, useAnimationControls } from "framer-motion";
 import localFont from "@next/font/local";
-
+import ClientLayout from "./layout-client";
 import "./styles.css";
 
 const font = localFont({ src: "../public/fonts/Synonym-Variable.woff2" });
 
-const RootLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
-  const controls = useAnimationControls();
-  useEffect(() => {
-    controls.start("visible");
-    // controls.start("hidden").then(() => controls.start("visible")); // Probably will need this in the future
-  }, [controls]);
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en" className={font.className}>
       <head />
       <body className="text-daw-main-600">
-        <motion.div
-          className="container max-w-screen-md"
-          initial="hidden"
-          animate={controls}
-          variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
-        >
-          {children}
-        </motion.div>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
-};
-
-export default RootLayout;
+}
