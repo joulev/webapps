8f2d43f8d39659b35e0603e102ab7a1e94ed59a6
Vu Van Dung <me@joulev.dev>
2022-10-27 16:24:56 +0800

    [apps/website] fix build error (although deploy somehow succeeded?)
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,11 +1,13 @@
+// @ts-check
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  rewrites: () => [
-    { source: "/cv", destination: process.env.RESUME_URL },
-    { source: "/cv.pdf", destination: process.env.RESUME_URL },
-    { source: "/resume", destination: process.env.RESUME_URL },
+  rewrites: async () => [
+    { source: "/cv", destination: process.env.RESUME_URL ?? "" },
+    { source: "/cv.pdf", destination: process.env.RESUME_URL ?? "" },
+    { source: "/resume", destination: process.env.RESUME_URL ?? "" },
   ],
   experimental: { appDir: true },
 };
