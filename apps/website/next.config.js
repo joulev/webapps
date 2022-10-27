e747ee14c8c3406a692873e2d126d0c9509d5eee
Vu Van Dung <me@joulev.dev>
2022-10-27 16:14:07 +0800

    [apps/website] why is middleware not working in nextjs 13????
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
+  rewrites: () => [
+    { source: "/cv", destination: process.env.RESUME_URL },
+    { source: "/cv.pdf", destination: process.env.RESUME_URL },
+    { source: "/resume", destination: process.env.RESUME_URL },
+  ],
+  experimental: { appDir: true },
 };
 
 module.exports = nextConfig;
