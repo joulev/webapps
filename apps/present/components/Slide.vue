7e3d5806850007347df7c5c99d60cb7b88aad16c
Vu Van Dung <me@joulev.dev>
2022-10-29 01:45:21 +0800

    [apps/present] n>1 slides now possible
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/present/components/Slide.vue
+++ apps/present/components/Slide.vue
@@ -1,22 +1,19 @@
 <script lang="ts" setup>
-defineProps<{
-  title?: string; // or a component type?
-  inset: { x: string; y: string };
-  space: string[];
-}>();
+defineProps<{ title?: string; slide: number }>();
+
+const space = inject<string[]>("space")!;
+const { x, y } = inject<{ x: string; y: string }>("inset")!;
+const current = inject<number>("current")!;
 </script>
 
 <template>
-  <div class="relative w-screen h-screen bg-[black]">
-    <div
-      class="absolute bg-main-900 text-main-100 flex flex-col"
-      style="box-shadow: 0px 0px 120px 0px rgba(120, 113, 108, 0.5)"
-      :style="{ inset: `${inset.y} ${inset.x}`, padding: space[2], gap: space[2] }"
-    >
-      <h1 v-if="title" class="font-semibold" :style="{ fontSize: space[2] }">{{ title }}</h1>
-      <div :style="{ fontSize: space[1] }">
-        <slot />
-      </div>
-    </div>
+  <div
+    class="absolute bg-main-900 text-main-100 flex flex-col"
+    style="box-shadow: 0px 0px 120px 0px rgba(120, 113, 108, 0.5)"
+    :style="{ inset: `${y} ${x}`, padding: space[2], gap: space[2] }"
+    v-if="current === slide"
+  >
+    <h1 v-if="title" class="font-semibold" :style="{ fontSize: space[2] }">{{ title }}</h1>
+    <div :style="{ fontSize: space[1] }"><slot /></div>
   </div>
 </template>
--- apps/present/pages/slides/index.vue
+++ apps/present/pages/slides/index.vue
@@ -1,20 +1,46 @@
 <script lang="ts" setup>
 const { x, y, gap } = useSlideInset();
 const space = computed(() => new Array(6).fill(0).map((_, i) => `${gap.value * i}px`));
+
+const current = ref(0);
+const maxSlide = 1; // have to update this manually
+onMounted(() => {
+  window.addEventListener("keydown", e => {
+    if (e.key === "ArrowRight") current.value = Math.min(current.value + 1, maxSlide);
+    else if (e.key === "ArrowLeft") current.value = Math.max(current.value - 1, 0);
+  });
+});
+
+provide("space", space);
+provide("inset", { x, y });
+provide("current", current);
 </script>
 
 <template>
-  <Slide title="Hello, world!" :inset="{ x, y }" :space="space">
-    <ul class="list-disc">
-      <li>This is the first slide.</li>
-      <li>It has a title.</li>
-      <li>It has a list.</li>
-      <li>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores perferendis et minus,
-        excepturi animi repudiandae accusantium nemo numquam eos quam?
-      </li>
-    </ul>
-  </Slide>
+  <div class="relative w-screen h-screen bg-[black]">
+    <Slide :slide="0" title="Hello, world!">
+      <ul class="list-disc">
+        <li>This is the first slide.</li>
+        <li>It has a title.</li>
+        <li>It has a list.</li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores perferendis et minus,
+          excepturi animi repudiandae accusantium nemo numquam eos quam?
+        </li>
+      </ul>
+    </Slide>
+    <Slide :slide="1" title="Hello, world! 2">
+      <ul class="list-disc">
+        <li>This is the second slide.</li>
+        <li>It has a title.</li>
+        <li>It has a list.</li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores perferendis et minus,
+          excepturi animi repudiandae accusantium nemo numquam eos quam?
+        </li>
+      </ul>
+    </Slide>
+  </div>
 </template>
 
 <style scoped>
