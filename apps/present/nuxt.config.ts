bd676d63ba595d9732e2a68ea84a5c2b36a58753
Vu Van Dung <me@joulev.dev>
2022-10-28 23:02:30 +0800

    [apps/present] use same nuxt config as apps/link
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/present/nuxt.config.ts
+++ apps/present/nuxt.config.ts
@@ -1,4 +1,43 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  typescript: { typeCheck: "build", strict: true },
+  nitro: { preset: "vercel" },
+  app: {
+    head: {
+      meta: [
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:creator", content: "@joulev_3" },
+        { property: "og:type", content: "website" },
+        { property: "og:image", content: "https://static.joulev.dev/images/og-image.png" },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:image:height", content: "630" },
+      ],
+      link: [
+        {
+          rel: "icon",
+          href: "https://static.joulev.dev/images/favicon.svg",
+          type: "image/svg+xml",
+        },
+        {
+          rel: "icon",
+          href: "https://static.joulev.dev/favicon.ico",
+          type: "image/x-icon",
+          sizes: "any",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "https://static.joulev.dev/images/apple-touch-icon.png",
+          sizes: "180x180",
+        },
+        {
+          rel: "preload",
+          href: "https://static.joulev.dev/fonts/Synonym-Variable.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
 });
