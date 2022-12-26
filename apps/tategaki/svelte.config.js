d54bbfea67a86e09609e06f0f6046312b14ff6d6
Vu Van Dung <me@joulev.dev>
2022-12-26 14:33:14 +0700

    [apps/{anime,tategaki}] use adapter-vercel
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/tategaki/svelte.config.js
+++ apps/tategaki/svelte.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
