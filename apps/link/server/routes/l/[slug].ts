35a3350a4924ce6edce0d0d285e58c1f3573b909
Vu Van Dung <me@joulev.dev>
2023-02-16 01:55:58 +0800

    [apps/link] fix ts error
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/link/server/routes/l/[slug].ts
+++ apps/link/server/routes/l/[slug].ts
@@ -2,8 +2,8 @@ import { client } from "~/server/lib/mongo";
 import { LinkDocument } from "~/types";
 
 export default defineEventHandler(async event => {
-  const slug = event.context.params.slug as string;
-  if (slug === "") return;
+  const slug = event.context.params?.slug;
+  if (!slug) return;
   try {
     const collection = client.db("link").collection<LinkDocument>("links");
     const entry = await collection.findOne({ slug });
