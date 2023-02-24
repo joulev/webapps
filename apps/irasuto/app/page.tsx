aa56c72ed7a44646ed4e2b9e0823c49c9da476a1
Vu Van Dung <me@joulev.dev>
2023-02-24 22:11:17 +0800

    [apps/irasuto] add metadata
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/app/page.tsx
+++ apps/irasuto/app/page.tsx
@@ -38,3 +38,37 @@ export default async function Page() {
 }
 
 export const revalidate = 86400; // 24 hours
+
+const title = "irasuto at joulev.dev";
+const description =
+  "This is where I store my collection of some of the most gorgeous illustrations related to Japanese popular culture that I've found on Twitter.";
+const url = "https://irasuto.joulev.dev";
+export const metadata = {
+  title,
+  description,
+  robots: { index: false },
+  twitter: { card: "summary_large_image", creator: "@joulev_3" },
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: title,
+    images: [
+      {
+        url: "https://static.joulev.dev/api/og?title=irasuto",
+        alt: title,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  alternates: { canonical: url },
+  icons: {
+    icon: [
+      { url: "https://static.joulev.dev/images/favicon.svg", type: "image/svg+xml" },
+      { url: "https://static.joulev.dev/favicon.ico", type: "image/x-icon", sizes: "any" },
+    ],
+    apple: [{ url: "https://static.joulev.dev/images/apple-touch-icon.png", sizes: "180x180" }],
+  },
+};
