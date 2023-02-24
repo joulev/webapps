6105dbe8f23e4b480e36697f2f5f6220e18581dd
Vu Van Dung <me@joulev.dev>
2023-02-24 17:27:04 +0800

    [apps/irasuto] set up mongodb connection and prisma
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/app/page.tsx
+++ apps/irasuto/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
-  return <div className="text-red">Hello world</div>;
+import { prisma } from "~/lib/db";
+
+export default async function Page() {
+  const illustrations = (await prisma.illustration.findMany()).map(i => i.url);
+  return <pre className="text-sm">{JSON.stringify(illustrations, null, 2)}</pre>;
 }
