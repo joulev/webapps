30896a3035bb21b88b126cb84d233ff4ef48c7fe
Vu Van Dung <me@joulev.dev>
2023-02-24 21:09:26 +0800

    [apps/irasuto] change database schema to store less info
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/lib/utils.ts
+++ apps/irasuto/lib/utils.ts
@@ -3,8 +3,7 @@ import { getTweet as _getTweet } from "next-tweet/api/get-tweet";
 import { TweetWithPhoto } from "~/types";
 
 export async function getTweet(illustration: Illustration): Promise<TweetWithPhoto> {
-  const urlObj = new URL(illustration.url);
-  const id = urlObj.pathname.split("/").at(-1);
+  const id = illustration.tweetId;
   if (!id) throw new Error();
   const tweet = await _getTweet(id);
   if (!tweet || !tweet.photos) throw new Error();
