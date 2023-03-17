ee760413271fd728d7025b2dc08eeef5e4d65e1f
Vu Van Dung <me@joulev.dev>
2023-03-17 10:37:14 +0800

    [apps/irasuto] enhance display of photo card metadata
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/app/tweet-photo.tsx
+++ apps/irasuto/app/tweet-photo.tsx
@@ -10,12 +10,12 @@ export default function TweetPhoto({ url, width, height, tweetUrl, author, dateA
         href={tweetUrl}
         className="absolute inset-0 flex flex-col justify-end p-6 bg-gradient-to-b from-main-900/20 to-main-900/90 opacity-0 group-hover:opacity-100 transition"
       >
-        <div className="flex flex-row justify-between items-end">
-          <div className="flex flex-col">
-            <div className="text-main-100">{author.name}</div>
+        <div className="flex flex-col justify-end">
+          <div className="text-main-100">{author.name}</div>
+          <div className="flex flex-row justify-between">
             <div className="help-text">@{author.handle}</div>
+            <div className="help-text">{dateAgo}</div>
           </div>
-          <div className="help-text">{dateAgo}</div>
         </div>
       </A>
     </div>
