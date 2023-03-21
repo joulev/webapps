b704da610225a1fedfbe5a0bfc0a18004679111b
Vu Van Dung <me@joulev.dev>
2023-03-21 13:37:49 +0800

    [apps/chatgpt] redirect on successful login
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/login/route.ts
+++ apps/chatgpt/app/login/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
   if (!secret || secret !== process.env.JOULEV_PASSWORD)
     return new Response("Incorrect credentials", { status: 401 });
 
-  return new Response("Ok!", {
+  return NextResponse.redirect(new URL("/", request.url), {
     headers: { "Set-Cookie": `secret=${secret}; Path=/; HttpOnly; SameSite=Lax; Secure` },
   });
 }
