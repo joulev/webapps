4142987915b1f599cc0978756f9454a48ecb6082
Vu Van Dung <me@joulev.dev>
2023-03-21 14:06:57 +0800

    [apps/chatgpt] disable zooming in on mobile
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/(authed)/layout.tsx
+++ apps/chatgpt/app/(authed)/layout.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   if (cookies().get("secret")?.value !== process.env.JOULEV_PASSWORD) redirect("/forbidden");
   return <>{children}</>;
 }
+
+// https://stackoverflow.com/a/46254706
+export const metadata: Metadata = {
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
