3acc1b52505599503f3798555072fc25a49e5368
Vu Van Dung <me@joulev.dev>
2023-03-26 17:40:30 +0800

    [apps/irasuto] send error info as res if revalidation fails
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/pages/api/revalidate.ts
+++ apps/irasuto/pages/api/revalidate.ts
@@ -7,6 +7,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await res.revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
-    return res.status(500).send("Error revalidating");
+    return res.status(500).send(err);
   }
 }
