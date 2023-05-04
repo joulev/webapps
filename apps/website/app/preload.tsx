be28cbf505d01590b7e8331e371b2d5f8df7c915
Vu Van Dung <me@joulev.dev>
2023-05-05 04:18:01 +0800

    (irasuto website) update font preload functions

--- apps/website/app/preload.tsx
+++ apps/website/app/preload.tsx
@@ -3,9 +3,10 @@
 import ReactDOM from "react-dom";
 
 export function Preload() {
-  // @ts-expect-error
   ReactDOM.preload("https://static.joulev.dev/fonts/Synonym-Variable.woff2", {
     as: "font",
+    crossOrigin: "",
+    // @ts-expect-error
     type: "font/woff2",
   });
   return null;
