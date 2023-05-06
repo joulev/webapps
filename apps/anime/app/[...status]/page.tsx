1175ddc329fadfc660415a332d0865fa7fcdcf01
Vu Van Dung <me@joulev.dev>
2023-05-06 17:34:19 +0700

    (anime) sort the lists properly

--- apps/anime/app/[...status]/page.tsx
+++ apps/anime/app/[...status]/page.tsx
@@ -1,4 +1,5 @@
 import { Item, getLists } from "~/lib/get-lists";
+import { getTitle } from "~/lib/utils";
 import Card, { CardVariant } from "~/app/_card/card";
 
 import EmptyState from "./empty-state";
@@ -14,6 +15,36 @@ type Params = {
     | ["dropped"]
     | ["planning"];
 };
+
+function sortList(list: (Item | null)[], type: "planning" | "completed" | "others") {
+  if (type === "planning")
+    return [...list].sort((a, b) =>
+      getTitle(a?.media?.title).localeCompare(getTitle(b?.media?.title)),
+    );
+  if (type === "completed")
+    return [...list].sort((a, b) => {
+      const aScore = a?.score ?? 0;
+      const bScore = b?.score ?? 0;
+      const titleCompare = getTitle(a?.media?.title).localeCompare(getTitle(b?.media?.title));
+
+      // if an entry is not scored yet, push it to the top
+      if (aScore === 0 && bScore === 0) return titleCompare;
+      if (aScore === 0) return -1;
+      if (bScore === 0) return 1;
+
+      // if both entries are scored, sort by score, from main score to each subscore
+      let cur_comp = bScore - aScore;
+      if (cur_comp !== 0) return cur_comp;
+      for (const key of Object.keys(a?.advancedScores ?? {})) {
+        cur_comp = (b?.advancedScores?.[key] ?? 0) - (a?.advancedScores?.[key] ?? 0);
+        if (cur_comp !== 0) return cur_comp;
+      }
+
+      // all are equal, break tie by title
+      return titleCompare;
+    });
+  return [...list].sort((a, b) => (b?.updatedAt ?? 0) - (a?.updatedAt ?? 0));
+}
 
 async function getList(status: Params["status"]): Promise<[(Item | null)[], CardVariant]> {
   const lists = await getLists();
@@ -42,9 +73,10 @@ async function getList(status: Params["status"]): Promise<[(Item | null)[], Card
 export default async function Page({ params }: { params: Params }) {
   const [list, variant] = await getList(params.status);
   if (list.length === 0) return <EmptyState />;
-  return list.map(item =>
-    item ? <Card item={item} variant={variant} key={item.mediaId} /> : null,
-  );
+  return sortList(
+    list,
+    variant === "planning" ? "planning" : variant.includes("completed") ? "completed" : "others",
+  ).map(item => (item ? <Card item={item} variant={variant} key={item.mediaId} /> : null));
 }
 
 export function generateStaticParams(): Params[] {
