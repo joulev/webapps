3ec45cf4f19550f63d5f305a8a15e0a3585b8546
Vu Van Dung <me@joulev.dev>
2023-06-13 12:50:33 +0700

    (chatgpt) set cookie max-age
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/login/route.ts
+++ apps/chatgpt/app/login/route.ts
@@ -6,8 +6,11 @@ export async function GET(request: NextRequest) {
   if (!secret || secret !== env.JOULEV_PASSWORD)
     return new Response("Incorrect credentials", { status: 401 });
 
+  const maxAge = 400 * 24 * 60 * 60; // 400 days, maximum possible for Chrome, higher values will still result in 400 days limit
   return NextResponse.redirect(new URL("/", request.url), {
-    headers: { "Set-Cookie": `secret=${secret}; Path=/; HttpOnly; SameSite=Lax; Secure` },
+    headers: {
+      "Set-Cookie": `secret=${secret}; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=${maxAge}`,
+    },
   });
 }
 
