72be1aefffbe15b039d9aa9ef9f254989bad411c
Vu Van Dung <me@joulev.dev>
2023-06-16 10:33:23 +0700

    (chatgpt) add preload font elements
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/layout.tsx
+++ apps/chatgpt/app/layout.tsx
@@ -1,9 +1,11 @@
+import { Preload } from "./preload";
 import "./styles.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Preload />
     </html>
   );
 }
