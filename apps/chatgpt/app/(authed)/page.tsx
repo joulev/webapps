96c249ca77f3f76dee0ce759cd0ce6e32880789e
Vu Van Dung <me@joulev.dev>
2023-06-16 09:17:59 +0700

    (chatgpt) use vercel ai sdk and get rid of spaghetti
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/(authed)/page.tsx
+++ apps/chatgpt/app/(authed)/page.tsx
@@ -1,27 +1,7 @@
 "use client";
 
+import { useChat } from "ai/react";
 import { useCallback, useState } from "react";
-
-type Message = {
-  role: "user" | "assistant";
-  content: string;
-};
-
-function getAnswerFromStreamResponse(raw: string) {
-  const messages = raw.split("\n");
-  let buffer = "";
-  for (const message of messages) {
-    try {
-      if (!message.startsWith("data: ")) continue;
-      const data = JSON.parse(message.slice("data: ".length));
-      if (data.object !== "chat.completion.chunk") continue;
-      buffer += (data.choices[0].delta.content as string | undefined) ?? "";
-    } catch {
-      continue;
-    }
-  }
-  return buffer;
-}
 
 function UserMessage({ content }: { content: string }) {
   return (
@@ -44,51 +24,20 @@ function ResponseMessage({ content }: { content: string }) {
 }
 
 export default function Page() {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const { messages, append } = useChat({ api: "/chat" });
   const [prompt, setPrompt] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const addAnswer = useCallback(
-    (answer: string) =>
-      setMessages(messages => {
-        const lastMessage = messages.at(-1);
-        if (lastMessage && lastMessage.role === "assistant")
-          return messages.map((message, i) =>
-            i === messages.length - 1 ? { ...message, content: answer } : message,
-          );
-        else return [...messages, { role: "assistant", content: answer }];
-      }),
-    [setMessages],
-  );
-
   const submit = useCallback(async () => {
     setIsLoading(true);
-    const res = await fetch("/chat", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt, previousMessages: messages }),
-    });
-    if (!res.ok || !res.body) {
-      setIsLoading(false);
-      alert(`Something went wrong (status: ${res.status})`);
-      return;
-    }
-    setMessages(messages => [...messages, { role: "user", content: prompt }]);
-    setPrompt("");
-    const reader = res.body.getReader();
-    const decoder = new TextDecoder();
-    let content = "";
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
-      const raw = decoder.decode(value);
-      content += raw;
-      addAnswer(getAnswerFromStreamResponse(content));
-      // Yeah I know it's not good; but this app can only be used by me so this is "good enough"
-      window.scrollTo(0, document.body.scrollHeight);
+    try {
+      await append({ role: "user", content: prompt });
+      setPrompt("");
+    } catch {
+      alert("Fetch failed, please try again later.");
     }
     setIsLoading(false);
-  }, [prompt, messages, addAnswer]);
+  }, [append, prompt]);
 
   const onKeyDown = useCallback(
     async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -113,7 +62,7 @@ export default function Page() {
         <div className="flex flex-col gap-3">
           <textarea
             className="w-full input h-24"
-            value={prompt}
+            value={isLoading ? "" : prompt}
             onKeyDown={onKeyDown}
             onChange={e => setPrompt(e.target.value)}
             disabled={isLoading}
--- apps/chatgpt/app/chat/route.ts
+++ apps/chatgpt/app/chat/route.ts
@@ -1,3 +1,4 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai-edge";
@@ -8,11 +9,18 @@ const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
 const inputSchema = z.object({
-  prompt: z.string(),
-  previousMessages: z.array(
-    z.object({ role: z.literal("assistant").or(z.literal("user")), content: z.string() }),
+  messages: z.array(
+    z.object({
+      id: z.string().optional(),
+      createdAt: z.any().optional(), // we don't care about this field
+      content: z.string(),
+      role: z.literal("system").or(z.literal("user")).or(z.literal("assistant")),
+    }),
   ),
 });
+
+const initialPrompt =
+  "You are a helpful assistant. The person you are talking to is a techy person, so please feel free to use technical terms without too lengthy explanation, if the person does not understand he will ask you back. Also, please do not reply in too long messages, and instead try to summarise the message as much as possible so that all important information is not lost while minimal space is given for not-so-helpful information.";
 
 export async function POST(request: Request) {
   const secret = cookies().get("secret")?.value;
@@ -20,21 +28,17 @@ export async function POST(request: Request) {
 
   const result = inputSchema.safeParse(await request.json());
   if (!result.success) return NextResponse.json({}, { status: 400 });
-  const { prompt, previousMessages } = result.data;
+  const { messages } = result.data;
 
   try {
-    const completion = await openai.createChatCompletion({
+    const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        ...previousMessages,
-        { role: "user", content: prompt },
-      ],
+      messages: [{ role: "system", content: initialPrompt }, ...messages],
       temperature: 0.2,
       stream: true,
     });
-
-    return new Response(completion.body);
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
   } catch (e) {
     console.error(e);
     return NextResponse.json({}, { status: 500 });
