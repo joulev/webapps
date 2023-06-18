8502540788f96180728409195d6b5b3b32457170
Vu Van Dung <me@joulev.dev>
2023-06-18 21:00:42 +0700

    (website) add redirect path to github sponsor page
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
     { source: "/cv.pdf", destination: env.RESUME_URL },
     { source: "/resume", destination: env.RESUME_URL },
   ],
+  redirects: async () => [
+    { source: "/sponsor", destination: "https://github.com/sponsors/joulev", permanent: false },
+  ],
 };
 
 export default nextConfig;
