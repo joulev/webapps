a17c12e38e20ddcc62e691d9fde8f503072e1ce9
Vu Van Dung <me@joulev.dev>
2023-07-01 15:02:46 +0700

    (irasuto) stop relying on twitter API, step 2
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/irasuto/app/add/route.ts
+++ apps/irasuto/app/add/route.ts
@@ -1,25 +1,64 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { env } from "~/env.mjs";
 import { prisma } from "~/lib/db";
-import { getTweet } from "~/lib/utils";
+import { convertUrlToPhotoId, getTweet } from "~/lib/utils";
 
 const schema = z.object({ password: z.literal(env.JOULEV_PASSWORD), url: z.string().url() });
+
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: { accessKeyId: env.R2_ACCESS_KEY, secretAccessKey: env.R2_SECRET_ACCESS_KEY },
+});
+
+async function uploadPhoto(url: string, tweetUrl: string) {
+  const id = convertUrlToPhotoId(url);
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(`Failed to download photo for id=${id} (${tweetUrl})`);
+
+  const arrayBuffer = await res.arrayBuffer();
+  const metadata = res.headers.get("content-type");
+  const buffer = Buffer.from(arrayBuffer);
+
+  const result = await S3.send(
+    new PutObjectCommand({
+      Bucket: "webapps-irasuto",
+      Key: `irasuto/${id}`,
+      Body: buffer,
+      ContentType: metadata ?? undefined,
+    }),
+  );
+  if (result.$metadata.httpStatusCode !== 200)
+    throw new Error(`Failed to upload photo for id=${id} (${tweetUrl})`);
+}
 
 export async function POST(request: Request) {
   const e = new Error();
   try {
     const url = new URL(schema.parse(await request.json()).url);
     if (url.hostname !== "twitter.com") throw e;
+
     const id = url.pathname.split("/").at(-1);
     if (!id) throw e;
+
     const tweetInfo = await getTweet({ id: "", tweetId: id });
     if (!tweetInfo) throw new Error();
+
+    const tweetUrl = `https://twitter.com/${tweetInfo.user.screen_name}/status/${tweetInfo.id_str}`;
+    await Promise.all(tweetInfo.photos.map(photo => uploadPhoto(photo.url, tweetUrl)));
+
     await prisma.illustration.create({ data: { tweetId: id } });
+
     revalidatePath("/");
+    fetch("https://irasuto.joulev.dev");
     revalidatePath("/newest");
+    fetch("https://irasuto.joulev.dev/newest");
+
     return new Response("Ok!");
-  } catch {
+  } catch (e) {
+    console.error(e);
     return new Response("Invalid request", { status: 400 });
   }
 }
--- apps/irasuto/env.mjs
+++ apps/irasuto/env.mjs
@@ -6,10 +6,16 @@ export const env = createEnv({
   server: {
     JOULEV_PASSWORD: z.string().min(1),
     MONGODB_URI: z.string().url(),
+    R2_ACCOUNT_ID: z.string().min(1),
+    R2_ACCESS_KEY: z.string().min(1),
+    R2_SECRET_ACCESS_KEY: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     JOULEV_PASSWORD: process.env.JOULEV_PASSWORD,
     MONGODB_URI: process.env.MONGODB_URI,
+    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
+    R2_ACCESS_KEY: process.env.R2_ACCESS_KEY,
+    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
   },
 });
