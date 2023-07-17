335d80e980a24a400afeb2708d4230ff93852964
Vu Van Dung <me@joulev.dev>
2023-07-17 15:21:12 +0700

    (chatgpt) add favicon for my Arc tab bar
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/chatgpt/app/layout.tsx
+++ apps/chatgpt/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { Preload } from "./preload";
 import "./styles.css";
 
@@ -9,3 +10,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://chatgpt.joulev.dev"),
+  title: "chatgpt at joulev.dev",
+  robots: { index: false, follow: false },
+  alternates: { canonical: "/" },
+  icons: {
+    icon: [
+      { url: "https://static.joulev.dev/images/favicon.svg", type: "image/svg+xml" },
+      { url: "https://static.joulev.dev/favicon.ico", type: "image/x-icon", sizes: "any" },
+    ],
+    apple: [{ url: "https://static.joulev.dev/images/apple-touch-icon.png", sizes: "180x180" }],
+  },
+};
